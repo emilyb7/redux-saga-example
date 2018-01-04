b736ed836a05d1e57e5af96703cb5e00a140205b
emily <limeyb7@gmail.com>
2018-01-04 22:15:49 +0000

    add function to get data + tests

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
+import { call, put } from 'redux-saga/effects'
+import createSagaMiddleware from 'redux-saga'
+
+const API = 'http://api.giphy.com/v1/stickers/random?key=dc6zaTOxFJmzC'
+
+const request = url => {
+  return fetch(url)
+    .then(res => res.json())
+    .catch(e => console.log(e))
+}
+
+export function* fetchData() {
+  yield call(request, API)
+  yield put({ type: 'DATA_LOADED' })
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
+  render = () => (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  )
 }
 
 export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { put } from 'redux-saga/effects'
+import { fetchData } from './App'
+import mockData from './mock.json'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+const fetchMock = response => {
+  global.fetch = jest.fn().mockImplementation(
+    () =>
+      new Promise(resolve => {
+        resolve({ json: () => response })
+      })
+  )
+}
+
+test('fetch data returns some data', async () => {
+  expect.assertions(4)
+
+  fetchMock(mockData)
+
+  const gen = fetchData()
+  const nextState = gen.next()
+
+  expect(nextState.done).toBe(false)
+  await expect(nextState.value.CALL.fn()).resolves.toMatchObject(mockData)
+
+  expect(gen.next().value).toMatchObject(put({ type: 'DATA_LOADED' }))
+  expect(gen.next().done).toBe(true)
+})
